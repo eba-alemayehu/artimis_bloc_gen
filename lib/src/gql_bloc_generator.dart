e44f812b184043139cfbf534720ff53c321c0447
eba-alemayehu <31594849+eba-alemayehu@users.noreply.github.com>
2021-12-15 15:27:46 +0300

    load more generated

--- lib/src/gql_bloc_generator.dart
+++ lib/src/gql_bloc_generator.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
@@ -12,35 +13,135 @@ class GqlBlocGenerator extends Generator {
   @override
   FutureOr<String> generate(LibraryReader library, BuildStep buildStep) {
     var buffer = StringBuffer();
-    var imports = StringBuffer();
-    imports.writeln('import \'package:flutter_bloc/flutter_bloc.dart\';');
-    imports.write(
-        'import \'package:${builderOptions.config['graphql_client']['import'].toString()}\';');
-    updateImports(buildStep, imports.toString());
-    final className = library.classes
-        .firstWhere((e) =>
-            e.displayName.contains('\$Query') ||
-            e.displayName.contains('\$Mutation'))
-        .displayName;
+    imports(library, buildStep);
+
+    final className = getClass(library).displayName;
 
     String sourceCode = template;
-    if (library.classes
-        .where((e) => e.displayName.contains('Arguments'))
-        .isEmpty) {
-      buffer.writeln('/*${library.classes}*/');
-      sourceCode = sourceCode
-          .replaceAll('variables: event.args', '');
+
+    if (getArgumentsClass(library) == null) {
+      sourceCode = clear_argumetns(sourceCode);
     }
+    sourceCode = hydratedEditor(library, sourceCode);
     sourceCode = sourceCode
-        .replaceAll('TemplateQuery', className.replaceAll('\$', ''))
-        .replaceAll('Template', className)
+        .replaceAll('TemplateQuery',
+            "${getClassName(library)}${isQuery(library) ? 'Query' : 'Mutation'}")
+        .replaceAll('Template', getClassName(library))
         .replaceAll('GraphQL.instance',
-            builderOptions.config['graphql_client']['object'].toString())
-        .replaceAll('\$Query', '')
-        .replaceAll('\$Mutation', '');
+            builderOptions.config['graphql_client']['object'].toString());
+
     buffer.writeln(sourceCode);
 
     return "${buffer.toString()}";
+  }
+
+  String clear_argumetns(String sourceCode) {
+    sourceCode = sourceCode.replaceAll('variables: event.args', '');
+    sourceCode = sourceCode.replaceAll('final TemplateArguments? args;', '');
+    sourceCode =
+        sourceCode.replaceAll('final TemplateArguments? withArgs;', '');
+    sourceCode = sourceCode.replaceAll('TemplateArguments args;', '');
+    sourceCode = sourceCode.replaceAll('TemplateArguments withArgs;', '');
+    sourceCode = sourceCode.replaceAll('TemplateArguments? args;', '');
+    sourceCode = sourceCode.replaceAll('TemplateArguments? withArgs;', '');
+
+    sourceCode = sourceCode.replaceAll(', this.withArgs', '');
+    sourceCode = sourceCode.replaceAll(', this.args', '');
+    sourceCode = sourceCode.replaceAll('this.withArgs', '');
+    sourceCode = sourceCode.replaceAll('this.args', '');
+    sourceCode = sourceCode.replaceAll('event.withArgs', '');
+    sourceCode = sourceCode.replaceAll('event.args', '');
+    sourceCode = sourceCode.replaceAll('withArgs', '');
+    sourceCode = sourceCode.replaceAll('args', '');
+    return sourceCode;
+  }
+
+  ClassElement getClass(LibraryReader library) {
+    return library.classes.firstWhere((e) =>
+        e.displayName.contains('\$Query') ||
+        e.displayName.contains('\$Mutation'));
+  }
+
+  getArgumentsClass(LibraryReader library) {
+    final argumentClass = library.classes
+        .firstWhere((e) =>
+            e.displayName == getClass(library).displayName.replaceAll("\$", ""))
+        .fields
+        .where((e) => e.type.toString().endsWith("Arguments"));
+    return (argumentClass.isEmpty) ? null : argumentClass.first;
+  }
+
+  String getClassName(LibraryReader library) {
+    return getClass(library)
+        .displayName
+        .replaceAll('\$Query', '')
+        .replaceAll('\$Mutation', '');
+  }
+
+  void imports(LibraryReader library, buildStep) {
+    var imports = StringBuffer();
+    imports.writeln('import \'package:' +
+        ((isQuery(library))
+            ? 'hydrated_bloc/hydrated_bloc.dart\';'
+            : 'flutter_bloc/flutter_bloc.dart\';'));
+
+    imports.write(
+        'import \'package:${builderOptions.config['graphql_client']['import'].toString()}\';');
+    updateImports(buildStep, imports.toString());
+  }
+
+  String hydratedEditor(LibraryReader library, String sourceCode) {
+    try {
+      FieldElement nodeField = getClass(library).fields.firstWhere((e) {
+        return e.type.toString().contains("Node") ||
+            e.type.toString().contains("Payload") ||
+            e.type.toString().contains("Mutation");
+      });
+
+      if (isQuery(library) &&
+          nodeField.type.toString().contains("Connection")) {
+        sourceCode = sourceCode
+            .replaceAll('Bloc<', 'HydratedBloc<')
+            .replaceAll('// fromJsonPlaceholder', fromJsonTemplate)
+            .replaceAll('// toJsonPlaceholder', toJsonTemplate);
+        if (getArgumentsClass(library) == null) {
+          sourceCode = sourceCode.replaceAll(
+              'return TemplateLoadedState(TemplateNodeConnection.fromJson(json), null);',
+              'return TemplateLoadedState(TemplateNodeConnection.fromJson(json));');
+        }
+        if (hasPageInfo(library, nodeField) &&
+            getArgumentsClass(library) != null) {
+          sourceCode = loadMoreHandelerEdit(sourceCode);
+        }
+      }
+      sourceCode = sourceCode
+          .replaceAll('#rootNode', nodeField.displayName)
+          .replaceAll('TemplateNodeConnection',
+              nodeField.type.toString().replaceAll("?", ""));
+      return sourceCode;
+    } catch (e) {
+      sourceCode = sourceCode.replaceAll('#rootNode', 'rootNode');
+      return sourceCode;
+    }
+  }
+
+  String loadMoreHandelerEdit(String sourceCode) {
+    sourceCode = sourceCode
+        .replaceAll(
+            '// loadMoreEventHandlerPlaceholder', loadMoreHandlerTemplate)
+        .replaceAll('// loadMoreMethodPlaceholder', loadMoreMethodTemplate)
+        .replaceAll('// loadMoreEventPlaceholder', loadMoreEventTemplate)
+        .replaceAll('// coursorUpdatePlaceholder', coursorUpdateTemplate);
+    return sourceCode;
+  }
+
+  bool hasPageInfo(LibraryReader library, FieldElement nodeField) {
+    return library.classes
+        .firstWhere((e) =>
+            e.displayName == nodeField.type.toString().replaceAll('?', ''))
+        .fields
+        .where((e) => e.displayName == "pageInfo")
+        .isNotEmpty;
   }
 
   Future<void> updateImports(buildStep, imports) async {
@@ -66,17 +167,24 @@ class GqlBlocGenerator extends Generator {
         client.then((client) => client
             .execute(TemplateQuery(variables: event.args))
             .then((response) => (response.errors == null)
-            ? this.add(TemplateLoadedEvent(response))
+            ? this.add(TemplateLoadedEvent(response.data?.#rootNode, event.args))
             : this.add(TemplateErrorEvent(response.errors)))
             .catchError((error) => this.add(TemplateExceptionEvent(error))));
+      // loadMoreEventHandlerPlaceholder
       } else if (event is TemplateLoadedEvent){
-         yield TemplateLoadedState(event.response);
+         yield TemplateLoadedState(event.#rootNode, event.withArgs);
       } else if (event is TemplateErrorEvent){
         yield TemplateErrorState(event.errors);
       } else if (event is TemplateExceptionEvent){
         yield TemplateExceptionState(event.exception);
       }
     }
+    
+    // loadMoreMethodPlaceholder
+
+    // fromJsonPlaceholder
+    
+    // toJsonPlaceholder
   }
   
   // Events
@@ -86,13 +194,14 @@ class GqlBlocGenerator extends Generator {
   
   
   class LoadTemplateEvent extends TemplateEvent {
-    final args;
+    TemplateArguments args;
     LoadTemplateEvent(this.args);
   
     @override
     List<Object> get props => [args];
   }
   
+  // loadMoreEventPlaceholder
   class LoadingTemplateEvent extends TemplateEvent {
     LoadingTemplateEvent();
   
@@ -101,12 +210,23 @@ class GqlBlocGenerator extends Generator {
   }
   
   class TemplateLoadedEvent extends TemplateEvent {
-    final response;
-  
-    TemplateLoadedEvent(this.response);
+    final #rootNode;
+    final TemplateArguments? withArgs;
+    
+    TemplateLoadedEvent(this.#rootNode, this.withArgs);
   
     @override
-    List<Object> get props => [response];
+    List<Object> get props => [#rootNode];
+  }
+  
+  class TemplateLoadedMoreEvent extends TemplateEvent {
+    final #rootNode;
+    final TemplateArguments? withArgs;
+    
+    TemplateLoadedMoreEvent(this.#rootNode, this.withArgs);
+  
+    @override
+    List<Object> get props => [#rootNode];
   }
   
   class TemplateErrorEvent extends TemplateEvent {
@@ -142,12 +262,13 @@ class GqlBlocGenerator extends Generator {
   
   
   class TemplateLoadedState extends TemplateState {
-    final response;
+    TemplateNodeConnection #rootNode;
+    final TemplateArguments? withArgs;
   
-    TemplateLoadedState(this.response);
+    TemplateLoadedState(this.#rootNode, this.withArgs);
     
     @override
-    List<Object> get props => [response];
+    List<Object> get props => [#rootNode];
   }
   
   
@@ -169,4 +290,73 @@ class GqlBlocGenerator extends Generator {
     @override
     List<Object> get props => [this.exception];
   }""";
+
+  late String fromJsonTemplate = """
+  @override
+  TemplateState? fromJson(Map<String, dynamic> json) {
+    try {
+      return TemplateLoadedState(TemplateNodeConnection.fromJson(json), null);
+    } catch (_) {
+      return null;
+    }
+  }
+  """;
+
+  late String toJsonTemplate = """
+  @override
+  Map<String, dynamic>? toJson(TemplateState state) {
+    if (state is TemplateLoadedState) {
+      return state.#rootNode.toJson();
+    } else {
+      return null;
+    }
+  }
+  """;
+
+  late String loadMoreEventTemplate = """
+  class LoadMoreTemplateEvent extends TemplateEvent {
+    final TemplateArguments? args;
+    LoadMoreTemplateEvent(this.args);
+  
+    @override
+    List<Object> get props => [];
+  }
+  """;
+
+  late String loadMoreHandlerTemplate = """
+   } else if (event is LoadMoreTemplateEvent) {
+        final state = this.state;
+        if (state is TemplateLoadedState) {
+          final client = GraphQL.instance;
+          dynamic args =  state.withArgs;
+          // coursorUpdatePlaceholder
+          client.then((client) => client
+              .execute(TemplateQuery(variables: args))
+              .then((response) => (response.errors == null)
+                  ? #rootNodeLoadedMore(response.data?.#rootNode, state)
+                  : this.add(TemplateErrorEvent(response.errors)))
+              .catchError(
+                  (error) => this.add(TemplateExceptionEvent(error))));
+        }
+  """;
+
+  late String loadMoreMethodTemplate = """
+  void #rootNodeLoadedMore(
+      TemplateNodeConnection? #rootNode, TemplateLoadedState state) {
+      #rootNode?.edges = state.#rootNode.edges + #rootNode.edges;
+      this.add(TemplateLoadedEvent(#rootNode, state.withArgs));
+    }
+  """;
+
+  late String coursorUpdateTemplate = """
+  if (state.#rootNode.pageInfo != null) {
+     args.startsWith = state.#rootNode.pageInfo.endCursor;
+  }
+  """;
+
+  bool isQuery(LibraryReader library) {
+    return library.classes
+        .where((e) => e.displayName.contains('\$Query'))
+        .isNotEmpty;
+  }
 }
